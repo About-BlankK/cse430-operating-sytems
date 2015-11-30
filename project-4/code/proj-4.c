147b0a6eca03d512c68fc44c103f5d020acc24d3
Man Do <DanielMoores@users.noreply.github.com>
2015-11-29 19:12:53 -0700

    Update proj-4.c
    Put in a few sleep(): so we can actually see what's going on.
    A way to keep track of all the counts.
    Let me know if you like the changes.

--- project-4/code/proj-4.c
+++ project-4/code/proj-4.c
@@ -5,7 +5,7 @@
  *  Project:
  *      Project 4
  *  Description:
- *      Reader and Writer.
+ *      Readers and Writers.
  *  Class:
  *      CSE 430 Operating Systems - Partha Dasgupta
  *  Organization:
@@ -13,16 +13,17 @@
  ============================================*/
 
 #include <stdio.h>
+#include <unistd.h>
 #include "sem.h"
 
 void reader();
 void writer();
 
-int wwc, wc, rwc, rc = 0;	//waiting write count, waiting count, reader waiting count, reader count
+int wwc, wc, rwc, rc = 0;	//writer waiting count, writer count, reader waiting count, reader count
 
-Semaphore *mutex = NULL;
-Semaphore *rsem = NULL;
-Semaphore *wsem = NULL;
+Semaphore *mutex;
+Semaphore *rsem;
+Semaphore *wsem;
 
 void reader();
 void writer();
@@ -51,55 +52,65 @@ int main()
 void reader(){
 	while(1){
 		//Reader Entry
-		printf("\n[reader #%p]\tentering CS\n", runQ->head);
+		printf("\n[reader #%p]\tentering\n", runQ->head);
 		P(mutex);
 		if(wwc>0 || wc>0){
 			printf("\n[reader #%p]\tneed to wait for a writer to finish.", runQ->head);
 			rwc++;
+			printf("\nwwc:%d \twrc:%d \twc:%d \trc:%d\n", wwc, rwc, wc, rc);
 			V(mutex);
 			P(rsem);
 			printf("\n[reader #%p]\twaking up.", runQ->head);
 			rwc--;
+			printf("\nwwc:%d \twrc:%d \twc:%d \trc:%d\n", wwc, rwc, wc, rc);
 		}
 		rc++;
+		printf("\nwwc:%d \twrc:%d \twc:%d \trc:%d\n", wwc, rwc, wc, rc);
 		if(rwc>0)
 			V(rsem);
 		else
 			V(mutex);
-		printf("\n[reader #%p]\tgoing into CS.", runQ->head);
-
+		//printf("\n[reader #%p]\tgoing into CS.", runQ->head);
+		sleep(1);
 		//Reader Exit
 		printf("\n[reader #%p]\texiting, get mutex", runQ->head);
 		P(mutex);
 		rc--;
 		printf("\n[reader #%p]\tleaving", runQ->head);
+		printf("\nwwc:%d \twrc:%d \twc:%d \trc:%d\n", wwc, rwc, wc, rc);
 		if(rc==0 && wwc>0)
 			V(wsem);
 		else
 			V(mutex);
+		sleep(1);
 	}
 }
 
 void writer(){
 	while(1){
 		//Writer Entry
-		printf("\n[writer #%p]\tentering CS\n", runQ->head);
+		printf("\n[writer #%p]\tentering\n", runQ->head);
 		P(mutex);
 		if(rc>0 || wc>0){
-			printf("\n[writer #%p]\tincreasing 1 to wwc.", runQ->head);
+			printf("\n[writer #%p]\twriter waiting count increasing 1.", runQ->head);
 			wwc++;
+			printf("\nwwc:%d \twrc:%d \twc:%d \trc:%d\n", wwc, rwc, wc, rc);
 			V(mutex);
 			P(wsem);
 			printf("\n[writer #%p]\twaking up.", runQ->head);
 			wwc--;
+			printf("\nwwc:%d \twrc:%d \twc:%d \trc:%d\n", wwc, rwc, wc, rc);
 		}
 		wc++;
+		printf("\nwwc:%d \twrc:%d \twc:%d \trc:%d\n", wwc, rwc, wc, rc);
 		V(mutex);
-		printf("\n[writer #%p]\tgoing into CS.", runQ->head);
+		sleep(1);
+		//printf("\n[writer #%p]\tgoing into CS.", runQ->head);
 		//Writer Exit
 		printf("\n[writer #%p]\texiting", runQ->head);
 		P(mutex);
 		wc--;
+		printf("\nwwc:%d \twrc:%d \twc:%d \trc:%d\n", wwc, rwc, wc, rc);
 		if(rwc>0){
 			printf("\n[writer #%p]\tremoving a reader.", runQ->head);
 			V(rsem);
@@ -112,21 +123,6 @@ void writer(){
 			printf("\n[writer #%p]\treleasing the resource.", runQ->head);
 			V(mutex);
 		}
+		sleep(1);
 	}
 }
-
-/*void printQ()
-{
-	int i = 0;
-    printf("\n[program state]\t\twwc:%d wrc:%d wc:%d rc:%d\n", wwc, rwc,
-	   wc, rc);
-    if (i < NUM_READER + NUM_WRITER) {
-	printf("  running process is ");
-	if (runQ >= 50)
-	    printf("a writer, ");
-	else
-	    printf("a reader, ");
-	printf("#%p\n", runQ);
-    }
-    i++;
-}*/
